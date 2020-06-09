e7dca705cf3abd43490a233c50e8599acc5c2723
Catter <nickbikkerprive@gmail.com>
2020-06-09 14:42:21 +0200

    Navbar, cards
    added a navbar and card styling

--- resources/views/webshop/index.blade.php
+++ resources/views/webshop/index.blade.php
@@ -1,85 +1,56 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
 
-        <title>Laravel</title>
+@section('title')
+Laravel Webshop
+@endsection
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-           
+@section('content')
+<div class="row">
+    <div class="col-md-3">
+        <div class="card">
+            <img src="https://img.gameme.eu/images/w4cj4C2UUpD3bfyio6QTqL.jpg" class="card-img-top image-responsive" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Vbucks kopen?</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor numquam porro, inventore nesciunt tempore dignissimos iste quod animi incidunt provident cum aspernatur laborum magnam. Harum, cupiditate minus? Ullam, ab beatae?</p>
+                <div class="price float-left">$10,99</div>
+                <a href="#" class="btn btn-success float-right"><i class="fas fa-plus"></i> Add to card</a>
+            </div>
         </div>
-    </body>
-</html>
+    </div>
+    <div class="col-md-3">
+        <div class="card">
+            <img src="https://img.gameme.eu/images/w4cj4C2UUpD3bfyio6QTqL.jpg" class="card-img-top image-responsive" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Vbucks kopen?</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor numquam porro, inventore nesciunt tempore dignissimos iste quod animi incidunt provident cum aspernatur laborum magnam. Harum, cupiditate minus? Ullam, ab beatae?</p>
+                <div class="price float-left">$10,99</div>
+                <a href="#" class="btn btn-success float-right"><i class="fas fa-plus"></i> Add to card</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card">
+            <img src="https://img.gameme.eu/images/w4cj4C2UUpD3bfyio6QTqL.jpg" class="card-img-top image-responsive" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Vbucks kopen?</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor numquam porro, inventore nesciunt tempore dignissimos iste quod animi incidunt provident cum aspernatur laborum magnam. Harum, cupiditate minus? Ullam, ab beatae?</p>
+                <div class="price float-left">$10,99</div>
+                <a href="#" class="btn btn-success float-right"><i class="fas fa-plus"></i> Add to card</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card">
+            <img src="https://img.gameme.eu/images/w4cj4C2UUpD3bfyio6QTqL.jpg" class="card-img-top image-responsive" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Vbucks kopen?</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor numquam porro, inventore nesciunt tempore dignissimos iste quod animi incidunt provident cum aspernatur laborum magnam. Harum, cupiditate minus? Ullam, ab beatae?</p>
+                <div class="price float-left">$10,99</div>
+                <a href="#" class="btn btn-success float-right"><i class="fas fa-plus"></i> Add to card</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
